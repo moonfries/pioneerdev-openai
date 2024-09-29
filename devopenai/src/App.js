3a8c7c1de1f11f5f4e5dc753069fc4b3296c826f
jakearanas <jakearanas@gmail.com>
2024-09-29 17:13:59 +0800

    apply homepage ui

--- devopenai/src/App.js
+++ devopenai/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="leftmenu">
+        <div className="left-menu-button">
+          Clear
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar">
+              </div>
+              <div className="message">
+                Prompt here
+              </div>
+            </div>
+            <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt">
+              </div>
+              <div className="message">
+                I am the answer
+              </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div className="chat-input-holder">
+          <input rows="1" className="chat-input-input" placeholder="Prompt anything here">
+
+          </input>
+        </div>
+      </section>
     </div>
   );
 }
