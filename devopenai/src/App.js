cf87c5aa82ae2f79852ee9a173f04a3f046355e4
jakearanas <22251753+moonfries@users.noreply.github.com>
2024-09-30 12:07:24 +0800

    add functionality

--- devopenai/src/App.js
+++ devopenai/src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import ChatMessage from './components/ChatMessage';
 import './normal.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [chatLog, setChatLog] = useState([{
+    user: "gpt",
+    message: "Start here"
+  }, {
+    user: "me",
+    message: "Start now"
+  }]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setChatLog([...chatLog, { user: "me", message: `${input}`}])
+    setInput("");
+  }
+
+
   return (
     <div className="App">
       <aside className="leftmenu">
@@ -11,33 +30,41 @@ function App() {
       </aside>
       <section className="chatbox">
         <div className="chat-log">
-          <div className="chat-message">
-            <div className="chat-message-center">
-              <div className="avatar">
-              </div>
-              <div className="message">
-                Prompt here
-              </div>
-            </div>
-            <div className="chat-message chatgpt">
-            <div className="chat-message-center">
-              <div className="avatar chatgpt">
-              </div>
-              <div className="message">
-                I am the answer
-              </div>
-            </div>
-            </div>
+          {chatLog.map((message, index) => (
+            <ChatMessage key={index} message={message} />
+          ))}
         </div>
-        </div>
+        <form onSubmit={handleSubmit}>
         <div className="chat-input-holder">
-          <input rows="1" className="chat-input-input" placeholder="Prompt anything here">
+          <input 
+            rows="1"
+             className="chat-input-input" 
+             placeholder="Prompt anything here"
+             value={input}
+            onChange={(e) => setInput(e.target.value)}
+          >
 
           </input>
         </div>
+        </form>
       </section>
     </div>
   );
 }
+
+// const ChatMessage = ({ message }) => {
+//   return (
+//     <div className={`chat-message ${message.user === "gpt" && "chatgpt"}`}>
+//       <div className="chat-message-center">
+//         <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}>
+        
+//         </div>
+//         <div className="message">
+//           {message.message}
+//        </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default App;
